42ee8a6f51ceec198f77c1d2f907d349b240d5fc
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-22 10:39:58 -0500

    Create form in TaskForm component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import TaskForm from "./components/TaskForm";
+
+// Components
 import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
-  const stateTasks = useSelector(state => state.tasks)
-  console.log(stateTasks)
-
   return (
     <div className="App">
       <h1>Hello World</h1>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 function TaskForm() {
-  return <div>TaskForm</div>;
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+      ></textarea>
+
+      <button>Save</button>
+    </form>
+  );
 }
 
 export default TaskForm;
