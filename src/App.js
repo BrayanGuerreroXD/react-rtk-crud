fb51ab6d62a47d3fac3dc1e9c5de3e3b19a7f09b
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-22 10:00:51 -0500

    Add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
-  const tasksState = useSelector(state => state.tasks)
-  console.log(tasksState)
+  const stateTasks = useSelector(state => state.tasks)
+  console.log(stateTasks)
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <TaskForm/>
+      <TaskList/>
     </div>
   );
 }
