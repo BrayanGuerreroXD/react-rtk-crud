34ae312db539f65d2f3338f1314bd996b7cf17e8
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-23 18:45:55 -0500

    Create editTask function

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTask } from "../features/tasks/taskSlice";
-import {v4 as uuid} from "uuid"
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
 
 function TaskForm() {
   const [task, setTask] = useState({
@@ -12,6 +12,8 @@ function TaskForm() {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
 
   const handleChange = (e) => {
     setTask({
@@ -22,12 +24,27 @@ function TaskForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTask({
-      ...task,
-      id: uuid()
-    }));
+
+    if (params.id) {
+      dispatch(
+        editTask(editTask(task))
+      )
+    } else {
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
+        })
+      );
+    }
     navigate("/");
   };
+
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+  }, []);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -36,11 +53,13 @@ function TaskForm() {
         type="text"
         placeholder="title"
         onChange={handleChange}
+        value={task.title}
       />
       <textarea
         name="description"
         placeholder="description"
         onChange={handleChange}
+        value={task.description}
       ></textarea>
 
       <button>Save</button>
--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -22,14 +22,22 @@ export const taskSlice = createSlice({
     addTask: (state, action) => {
       state.push(action.payload);
     },
+    editTask: (state, action) => {
+      const { id, title, description } = action.payload;
+      const taskFound = state.find((task) => task.id === id);
+      if (taskFound) {
+        taskFound.title = title;
+        taskFound.description = description;
+      }
+    },
     deleteTask: (state, action) => {
-      const taskFound = state.find(task => task.id === action.payload)
+      const taskFound = state.find((task) => task.id === action.payload);
       if (taskFound) {
         state.splice(state.indexOf(taskFound), 1);
       }
-    }
+    },
   },
 });
 
-export const { addTask, deleteTask } = taskSlice.actions;
+export const { addTask, editTask, deleteTask } = taskSlice.actions;
 export default taskSlice.reducer;
